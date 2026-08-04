415ccc63edf4d18975b251a77969f611f4d22907
Likitha <likithaghanta4@gmail.com>
2026-08-04 17:26:01 +0530

    Fix video stream URL

--- dashboard/src/lib/api.js
+++ dashboard/src/lib/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL = (
 ).replace(/\/$/, "");
 const AI_STREAM_BASE_URL = (
   import.meta.env.VITE_AI_STREAM_BASE_URL ||
-  "https://rebel-providers-defined-legislature.trycloudflare.com/video-feed"
+  "https://rebel-providers-defined-legislature.trycloudflare.com"
 ).replace(/\/$/, "");
 
 export const apiEndpoints = {
